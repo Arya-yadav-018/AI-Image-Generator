d356541b82a7559f607571cbdc30efcfb54e0277
Arya yadav <151600217+Arya-yadav-018@users.noreply.github.com>
2025-10-17 17:08:13 +0530

    Changed component name

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../components/Header'
-import Steps from '../components/steps'
+import Steps from '../components/Steps'
 import Descriptions from '../components/Descriptions'
 import Testimonials from '../components/Testimonials'
 import GenerateBtn from '../components/GenerateBtn'
